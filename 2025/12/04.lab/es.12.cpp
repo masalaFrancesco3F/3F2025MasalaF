c0cc7b916734c453fe5c6056258032097f5937a3
masalaFrancesco3F <masala.francesco.2023@itisgalileiroma.it>
2025-12-04 09:33:18 +0100

    lab

--- 2025/12/04.lab/es.12.cpp
+++ 2025/12/04.lab/es.12.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 using namespace std;
-#include <algorithm>
 int main (){
-    int eta1,eta2,eta3;
+    int eta1,eta2,eta3,etamag,etamin,diff;
     cout << "Inserisci l'età dei tre fratelli :";
     cin >> eta1 >> eta2 >> eta3;
-      int eta_maggiore = max({eta1, eta2, eta3});
-    int eta_minore   = min({eta1, eta2, eta3});
-    int differenza_assoluta = eta_maggiore - eta_minore;
-    cout << "La differenza di eta tra il maggiore e il minore e': " << differenza_assoluta << " anni." << endl;
+    etamag=eta1;
+    if (etamag < eta2){
+        etamag=eta2;
+        if (etamag < eta3){
+            etamag=eta3;
+        }
+    } else {
+        if (etamag < eta3){
+            etamag=eta3;
+        }
+    }
+    etamin=eta1;
+    if (etamin > eta2){
+        etamin=eta2;
+        if (etamin > eta3){
+            etamin=eta3;
+        }
+    } else {
+        if (etamin > eta3){
+            etamin=eta3;
+        }
+    }
+    diff = etamag - etamin;
+    cout << "La differenza di eta tra il maggiore e il minore e': " << diff << " anni." << endl;
     
     return 0;
 }
