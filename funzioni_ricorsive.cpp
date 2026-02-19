85baba4ce2bfb065ac7d154a722c7451f5c56afb
Spacy96 <francescoalbin@gmail.com>
2026-02-19 08:45:54 +0100

    compiti

--- funzioni_ricorsive.cpp
+++ funzioni_ricorsive.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 int fattoriale(int n) {
+    int ret=n*n-1;
     if (n == 0 || n == 1) {
         return 1; 
     }
-    return n * fattoriale(n - 1); 
+    return ret; 
+}
+int main() {
+    int numero;
+    cout << "Inserisci un numero per calcolare il fattoriale: ";
+    cin >> numero;
+    int risultato = fattoriale(numero);
+    cout << "Il fattoriale di " << numero << " è: " << risultato << endl;
+    return 0;
 }
