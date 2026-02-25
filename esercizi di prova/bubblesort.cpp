811a35409ee902bb04cd5d88632f142fc78e3597
masalaFrancesco3F <masala.francesco.2023@itisgalileiroma.it>
2026-02-25 17:30:19 +0100

    compiti

--- esercizi di prova/bubblesort.cpp
+++ esercizi di prova/bubblesort.cpp
@@ -3,21 +3,21 @@
 
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int array[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
+            if (array[j] > array[j + 1]) {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
             }
         }
     }
 }
 
-void stampaArray(int arr[], int n) {
+void stampaArray(int array[], int n) {
     for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+        cout << array[i] << " ";
     }
     cout << endl;
 }
