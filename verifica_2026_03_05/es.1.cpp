781c9e38b476999abffd5cc9f7deb6dd26acaeee
Spacy96 <francescoalbin@gmail.com>
2026-03-05 09:06:18 +0100

    verifica

---
+++ verifica_2026_03_05/es.1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void StampaMultipli(int v[], int n, int x) {
+    cout << "I multipli di " << x << " nell'array sono: ";
+    for (int i = 0; i < n; ++i) {
+        if (v[i] % x == 0) {
+            cout << v[i] << " ";
+        }
+    }
+    cout << endl;
+}   
+int main() {
+    int dati[] = {2, 5, 10, 15, 20, 25, 30};
+    int n = sizeof(dati) / sizeof(dati[0]);
+    int x;
+
+    cout << "Inserisci un numero per trovare i suoi multipli: ";
+    cin >> x;
+
+    StampaMultipli(dati, n, x);
+
+    return 0;
+}
---
+++ verifica_2026_03_05/es.5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void non_picchi(int v[], int n) {
+    cout << "I non-picchi nell'array sono: ";
+    for (int i = 0; i < n; ++i) {
+        if ((i == 0 || v[i] <= v[i - 1]) && (i == n - 1 || v[i] <= v[i + 1])) {
+            cout << v[i] << " ";
+        }
+    }
+    cout << endl;
+}
+int main() {
+    int dati[] = {1, 3, 2, 5, 4, 6, 7, 8, 9};
+    int n = sizeof(dati) / sizeof(dati[0]);
+
+    non_picchi(dati, n);
+
+    return 0;
+}
